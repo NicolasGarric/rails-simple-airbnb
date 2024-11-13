9fc6e64364fd8edcca807120f383cc76b25e0205
Nicolas Garric <nicolasdfgarric@gmail.com>
2024-11-13 16:50:16 +0100

    Exercice termine à moitie. Mise en page front et finalisation du back à faire

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,44 @@
 class FlatsController < ApplicationController
+  def new
+    @flat = Flat.new # Needed to instantiate the form_with
+  end
+
   def index
-    
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    # No need for app/views/flats/create.html.erb
+    redirect_to flats_path
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    # No need for app/views/flats/update.html.erb
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    # No need for app/views/flats/destroy.html.erb
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :content)
   end
 end
