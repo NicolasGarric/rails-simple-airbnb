788c32752355009e41943a700c9cc1f021adcbe1
Nicolas Garric <nicolasdfgarric@gmail.com>
2024-11-13 15:52:39 +0100

    Seed added with 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.destroy_all
+puts "Flat destroyed..."
+
+Flat.create!(
+  name: 'Little Budapest Hotel',
+  address: 'Budapest lol',
+  description: 'A small hotel in Budapest',
+  price_per_night: 1000,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Grand Budapest Hotel',
+  address: 'Budapest lol',
+  description: 'A grand hotel in Budapest',
+  price_per_night: 1_000_000_000,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Medium Budapest Hotel',
+  address: 'Budapest lol',
+  description: 'A medium hotel in Budapest',
+  price_per_night: 1_000_000,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Some Hotel',
+  address: 'Some',
+  description: 'Somewere in some',
+  price_per_night: 23,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Simple Hotel',
+  address: 'Simple city',
+  description: 'Somewere in simple city',
+  price_per_night: 121,
+  number_of_guests: 4
+)
+
+puts "#{Flat.count} flats created"
